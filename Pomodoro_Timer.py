f1bdc9a25d14330145f66889716bcf48f6f899a9
Ganesha Shiisa <ganesha.shiisa@gmail.com>
2021-03-16 23:11:02 +0900

    説明追加

--- Pomodoro_Timer.py
+++ Pomodoro_Timer.py
@@ -1,42 +1,54 @@
+"""Pomodoro Timer
 
-import wx
+Pomodoro Timerは、いわゆるポモドーロテクニック用のタイマーアプリケーションです。
+25分と5分のタイマーが用意されています。
+
+"""
+
 import time
+import wx
 
 
 class MainWindow(wx.Frame):
+    """
+    MainWindowはメインのウィンドウ設定
+    """
+
     def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, wx.ID_ANY, title=title, size=(350, 150))
+        wx.Frame.__init__(self, parent, wx.ID_ANY,
+                          title=title, size=(350, 150))
         panel = wx.Panel(self, wx.ID_ANY)
 
-        self.s_text_MM = wx.StaticText(panel, wx.ID_ANY, '{:>2}'.format(0))
+        self.s_text_mm = wx.StaticText(panel, wx.ID_ANY, '{:>2}'.format(0))
         s_text_colon = wx.StaticText(panel, wx.ID_ANY, ':')
-        self.s_text_SS = wx.StaticText(panel, wx.ID_ANY, '{:02}'.format(0))
-        font = wx.Font(30, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
-        self.s_text_MM.SetFont(font)
+        self.s_text_ss = wx.StaticText(panel, wx.ID_ANY, '{:02}'.format(0))
+        font = wx.Font(30, wx.FONTFAMILY_DEFAULT,
+                       wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
+        self.s_text_mm.SetFont(font)
         s_text_colon.SetFont(font)
         self.s_text_SS.SetFont(font)
 
-        button_Work_Start = wx.Button(panel, wx.ID_ANY, '作業 ▶')
-        button_Break_Start = wx.Button(panel, wx.ID_ANY, '休憩 ▶')
-        button_Pause = wx.Button(panel, wx.ID_ANY, '一時停止')
-        button_Stop = wx.Button(panel, wx.ID_ANY, '停止')
+        button_work_start = wx.Button(panel, wx.ID_ANY, '作業 ▶')
+        button_break_start = wx.Button(panel, wx.ID_ANY, '休憩 ▶')
+        button_pause = wx.Button(panel, wx.ID_ANY, '一時停止')
+        button_stop = wx.Button(panel, wx.ID_ANY, '停止')
 
         time_layout = wx.BoxSizer(wx.HORIZONTAL)
         time_layout.AddStretchSpacer(prop=1)
-        time_layout.Add(self.s_text_MM, proportion=2, flag=wx.ALIGN_CENTER)
+        time_layout.Add(self.s_text_mm, proportion=2, flag=wx.ALIGN_CENTER)
         time_layout.Add(s_text_colon, proportion=1, flag=wx.ALIGN_CENTER)
-        time_layout.Add(self.s_text_SS, proportion=2, flag=wx.ALIGN_CENTER)
+        time_layout.Add(self.s_text_ss, proportion=2, flag=wx.ALIGN_CENTER)
         time_layout.AddStretchSpacer(prop=1)
 
-        operation_layout = wx.GridSizer(rows=2, cols=2, gap=(0,0))
-        operation_layout.Add(button_Work_Start, flag=wx.EXPAND)
-        operation_layout.Add(button_Pause, flag=wx.EXPAND)
-        operation_layout.Add(button_Break_Start, flag=wx.EXPAND)
-        operation_layout.Add(button_Stop, flag=wx.EXPAND)
+        operation_layout = wx.GridSizer(rows=2, cols=2, gap=(0, 0))
+        operation_layout.Add(button_work_start, flag=wx.EXPAND)
+        operation_layout.Add(button_pause, flag=wx.EXPAND)
+        operation_layout.Add(button_break_start, flag=wx.EXPAND)
+        operation_layout.Add(button_stop, flag=wx.EXPAND)
 
         top_layout = wx.BoxSizer(wx.HORIZONTAL)
         top_layout.Add(time_layout, proportion=2, flag=wx.EXPAND)
-        top_layout.Add(operation_layout, proportion=1,flag=wx.EXPAND)
+        top_layout.Add(operation_layout, proportion=1, flag=wx.EXPAND)
 
         panel.SetSizer(top_layout)
 
@@ -49,12 +61,16 @@ class MainWindow(wx.Frame):
         self.Show()
 
     def main(self, event):
+        """
+        mainは、メイン処理です。
+        """
+        print(event)
         self.count = self.count + 1
-        elapsed_time = int(round(time.time() - self.base_time,0))
+        elapsed_time = int(round(time.time() - self.base_time, 0))
         print(self.count, elapsed_time)
         elapsed_time_mm, elapsed_time_ss = divmod(elapsed_time, 60)
-        self.s_text_MM.SetLabel('{:>2}'.format(elapsed_time_mm))
-        self.s_text_SS.SetLabel('{:02}'.format(elapsed_time_ss))
+        self.s_text_mm.SetLabel('{:>2}'.format(elapsed_time_mm))
+        self.s_text_ss.SetLabel('{:02}'.format(elapsed_time_ss))
 
 
 app = wx.App(False)
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Pomodoro-Timer
-Pomodoro Timer
+
+Pomodoro Timerは、いわゆる[ポモドーロテクニック](https://ja.wikipedia.org/wiki/%E3%83%9D%E3%83%A2%E3%83%89%E3%83%BC%E3%83%AD%E3%83%BB%E3%83%86%E3%82%AF%E3%83%8B%E3%83%83%E3%82%AF)用のタイマーアプリケーションです。
+25分と5分のタイマーが用意されています。
+ 
+# DEMO
+ 
+***
+
+# Features
+ 
+ポモドーロテクニック用のタイマーは他にもたくさんありますが、以下の点で優れています。
+* タイマーを一時停止することができる。
+* 途中で停止することができる。
+* PCをスリープや停止させない。（定期的にキーボード操作をします）
+
+# Requirement
+ 
+* python
+* wxPython
+  
+# Usage
+ 
+```bash
+python Pomodoro_timer.py
+```
+ 
+# Note
+ 
