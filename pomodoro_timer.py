ad524624203b493409b1a5614f9083267433afe1
Ganesha Shiisa <ganesha.shiisa@gmail.com>
2021-03-18 10:59:05 +0900

    タイマー実装

--- pomodoro_timer.py
+++ pomodoro_timer.py
@@ -13,6 +13,13 @@ class MainWindow(wx.Frame):
     """
     MainWindowはメインのウィンドウ設定
     """
+    STATUS_STOP = 0
+    STATUS_WORK = 10
+    STATUS_WORK_PAUSE = 11
+    STATUS_WORK_END = 19
+    STATUS_BREAK = 20
+    STATUS_BREAK_PAUSE = 21
+    STATUS_BREAK_END = 29
 
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, wx.ID_ANY,
@@ -26,12 +33,17 @@ class MainWindow(wx.Frame):
                        wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
         self.s_text_mm.SetFont(font)
         s_text_colon.SetFont(font)
-        self.s_text_SS.SetFont(font)
+        self.s_text_ss.SetFont(font)
 
         button_work_start = wx.Button(panel, wx.ID_ANY, '作業 ▶')
         button_break_start = wx.Button(panel, wx.ID_ANY, '休憩 ▶')
         button_pause = wx.Button(panel, wx.ID_ANY, '一時停止')
         button_stop = wx.Button(panel, wx.ID_ANY, '停止')
+
+        button_work_start.Bind(wx.EVT_BUTTON, self.evt_button_work_start)
+        button_break_start.Bind(wx.EVT_BUTTON, self.evt_button_break_start)
+        button_pause.Bind(wx.EVT_BUTTON, self.evt_button_pause)
+        button_stop.Bind(wx.EVT_BUTTON, self.evt_button_stop)
 
         time_layout = wx.BoxSizer(wx.HORIZONTAL)
         time_layout.AddStretchSpacer(prop=1)
@@ -57,6 +69,11 @@ class MainWindow(wx.Frame):
         self.timer.Start(1000)
         self.count = 0
         self.base_time = time.time()
+        self.pause_time = 0
+        self.pause_time_buf = 0
+        self.event = ""
+
+        self.status = self.STATUS_STOP
 
         self.Show()
 
@@ -64,13 +81,84 @@ class MainWindow(wx.Frame):
         """
         mainは、メイン処理です。
         """
-        print(event)
+        print(self.status, self.pause_time_buf)
+        self.event = event
         self.count = self.count + 1
-        elapsed_time = int(round(time.time() - self.base_time, 0))
-        print(self.count, elapsed_time)
-        elapsed_time_mm, elapsed_time_ss = divmod(elapsed_time, 60)
-        self.s_text_mm.SetLabel('{:>2}'.format(elapsed_time_mm))
-        self.s_text_ss.SetLabel('{:02}'.format(elapsed_time_ss))
+
+        if self.status == self.STATUS_WORK_PAUSE or self.status == self.STATUS_BREAK_PAUSE:
+            pause_time_tmp = time.time() - self.pause_time_buf
+        else:
+            pause_time_tmp = 0
+
+        remaining_time = int(
+            round(self.base_time + self.pause_time + pause_time_tmp - time.time(), 0))
+        if remaining_time < 0:
+            remaining_time = 0
+            if self.status == self.STATUS_WORK:
+                self.status = self.STATUS_WORK_END
+            elif self.status == self.STATUS_BREAK:
+                self.status = self.STATUS_BREAK_END
+
+        time_mm, time_ss = divmod(remaining_time, 60)
+        self.s_text_mm.SetLabel('{:>2}'.format(time_mm))
+        self.s_text_ss.SetLabel('{:02}'.format(time_ss))
+
+        if self.status == self.STATUS_WORK_END:
+            self.pause_time = 0
+            self.status = self.STATUS_STOP
+
+        if self.status == self.STATUS_BREAK_END:
+            self.pause_time = 0
+            self.status = self.STATUS_STOP
+
+    def evt_button_work_start(self, event):
+        """「work_start」ボタンが押された時の処理
+        """
+        self.event = event
+        if self.status == self.STATUS_WORK_PAUSE:
+            self.pause_time += time.time() - self.pause_time_buf
+        else:
+            self.base_time = time.time() + 25
+            self.pause_time = 0
+            self.pause_time_buf = 0
+
+        self.status = self.STATUS_WORK
+
+    def evt_button_break_start(self, event):
+        """「break_start」ボタンが押された時の処理
+        """
+        self.event = event
+        if self.status == self.STATUS_BREAK_PAUSE:
+            self.pause_time += time.time() - self.pause_time_buf
+        else:
+            self.base_time = time.time() + 5
+            self.pause_time = 0
+            self.pause_time_buf = 0
+
+        self.status = self.STATUS_BREAK
+
+    def evt_button_pause(self, event):
+        """「pause」ボタンが押された時の処理
+        """
+        self.event = event
+        if not (self.status == self.STATUS_WORK_PAUSE or self.status == self.STATUS_BREAK_PAUSE):
+            self.pause_time_buf = time.time()
+
+        if self.status == self.STATUS_WORK:
+            self.status = self.STATUS_WORK_PAUSE
+        elif self.status == self.STATUS_BREAK:
+            self.status = self.STATUS_BREAK_PAUSE
+
+        print("button_pause")
+
+    def evt_button_stop(self, event):
+        """「stop」ボタンが押された時の処理
+        """
+        self.event = event
+        self.status = self.STATUS_STOP
+        self.base_time = time.time()
+        self.pause_time = 0
+        self.pause_time_buf = 0
 
 
 app = wx.App(False)
